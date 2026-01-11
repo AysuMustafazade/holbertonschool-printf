7df3e0f5b9d0cb481901e23646f21ac690f395de
AysuMustafazade <mustafazadays16@gmail.com>
2026-01-11 14:16:52 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,36 @@
-#include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
+
+/**
+ * print_number - prints an integer
+ * @n: number to print
+ *
+ * Return: number of characters printed
+ */
+int print_number(int n)
+{
+	unsigned int num;
+	int count = 0;
+	char c;
+
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		count++;
+		num = -n;
+	}
+	else
+		num = n;
+
+	if (num / 10)
+		count += print_number(num / 10);
+
+	c = (num % 10) + '0';
+	write(1, &c, 1);
+	count++;
+
+	return (count);
+}
 
 /**
  * _printf - produces output according to a format
@@ -12,17 +42,22 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
-	char *str;
 	char c;
+	char *str;
 
-	if (!format)
+	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
 
 	while (*format)
 	{
-		if (*format == '%')
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			count++;
+		}
+		else
 		{
 			format++;
 			if (*format == '\0')
@@ -30,6 +65,7 @@ int _printf(const char *format, ...)
 				va_end(args);
 				return (-1);
 			}
+
 			if (*format == 'c')
 			{
 				c = (char)va_arg(args, int);
@@ -48,6 +84,10 @@ int _printf(const char *format, ...)
 					count++;
 				}
 			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				count += print_number(va_arg(args, int));
+			}
 			else if (*format == '%')
 			{
 				write(1, "%", 1);
@@ -59,11 +99,6 @@ int _printf(const char *format, ...)
 				write(1, format, 1);
 				count += 2;
 			}
-		}
-		else
-		{
-			write(1, format, 1);
-			count++;
 		}
 		format++;
 	}
