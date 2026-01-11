fc76de7c678a554d95b1da3423426ae1d83ec149
AysuMustafazade <mustafazadays16@gmail.com>
2026-01-11 14:07:38 +0000

    produces output according to a format

---
+++ _printf.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+
+    if (!format)
+        return (-1);
+
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == 'c')
+            {
+                char c = (char)va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                if (!str) str = "(null)";
+                while (*str)
+                {
+                    write(1, str, 1);
+                    str++;
+                    count++;
+                }
+            }
+            else if (*format == '%')
+            {
+                write(1, "%", 1);
+                count++;
+            }
+        }
+        else
+        {
+            write(1, format, 1);
+            count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+    return count;
+}
